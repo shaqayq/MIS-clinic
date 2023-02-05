abb1be012f136f30dbd8572167286e1c26eec286
shaqayq <shaqayq.darwazi@gmail.com>
2023-02-05 12:34:06 +0430

    create role.index page

--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -1,18 +1,21 @@
 @extends('layouts.app')
 @section('content')
-<div class="content-header">
+ <div class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0 text-dark">لیست کاربران</h1>
+            <h1 class="m-0 text-dark">لیست صلاحیت ها</h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-left">
-              <li class="breadcrumb-item"><a href="#">کاربران</a></li>
+              <li class="breadcrumb-item"><a href="#">صلاحیت ها</a></li>
               <li class="breadcrumb-item active">داشبورد ورژن 2</li>
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
+        @if(auth()->user()->role == 'Admin')
+         <a href="{{route('role.create')}}"><button class="btn btn-success"><i class="fa fa-plus"></i></button></a>
+         @endif
       </div><!-- /.container-fluid -->
     </div>
     <!-- /.content-header -->
@@ -26,37 +29,33 @@
               <h3 class="card-title"></h3>
             </div>
             <!-- /.card-header -->
-           <div class="card-body">
+            <div class="card-body">
               <table id="example" class="table table-bordered table-striped">
                 <thead>
                 <tr>
                   <th>نام</th>
-                  <th>تخلص</th>
-                  <th>ایمیل</th>
-                  <th>شماره تماس</th>
                   <th>صلاحیت</th>
                   <th>تنظیمات</th>
+
                 </tr>
                 </thead>
                 <tbody>
-                @foreach($user as $u)
+                @foreach($roles as $r)
                 <tr>
-                  <td>{{$u->name}}</td>
-                <td>{{$u->L_name}}</td>
-                <td>{{$u->email}}</td>
-                <td>۰{{$u->phone}}</td>
-                <td>{{$u->role}}</td>
+                <td>{{$r->P_name}}</td>
+                <td>{{$r->E_name}}</td>
                 <td>
-                  <div style="display: flex">
-                    <form action="{{route('user.destroy' ,['id '=>$u->id])}}" method="post">
-                      {{method_field('delete')}}
-                      @csrf
-                      <div class="btn-group btn-group-xs">
-                        <a href="{{route('user.edit',$u->id)}}" class="btn btn-sm btn-primary"> <i class="fa fa-edit" ></i></a>
-                      </div>
-                      <a href=""><button type="submit" class="btn btn-sm btn-danger"><i class="fa fa-trash"></i></button></a>
-                    </form>
-                  </div>
+                   <div style="display: flex">
+                      <form action="{{ route('role.destroy'  , ['id' => $r->id]) }}" method="post">
+                                {{ method_field('delete') }}
+                                {{ csrf_field() }}
+                        <div class="btn-group btn-group-xs" >
+                            <a href="{{ route('role.edit', $r->id) }}" class="btn btn-sm btn-primary"><i class="fa fa-edit"></i></a>
+                              </div>
+                              <button  type="submit" class="btn btn-sm btn-danger" disabled=""><i class="fa fa-trash"></i></button>
+                      </form>
+                    </div>
+
                 </td>
                 </tr>
                 @endforeach
@@ -65,10 +64,10 @@
             </div>
             <!-- /.card-body -->
           </div>
-            <!-- /.card-body -->
-          </div>
 
       </div><!-- /.container-fluid -->
     </section>
     <!-- /.content -->
+  
+
 @endsection
