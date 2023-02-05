42c3849b2d2bae53ad331b32d389e2b4304d2f91
shaqayq <shaqayq.darwazi@gmail.com>
2023-02-05 12:55:20 +0430

    add styles

--- database/migrations/2021_06_10_100944_create_jobs_table.php
+++ database/migrations/2021_06_10_100944_create_jobs_table.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Migrations\Migration;
 class CreateJobsTable extends Migration
 {
     /**
-     * Run the migrations.
+     * Run the migrations. 
      *
      * @return void
      */
