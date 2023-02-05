91413893a564eb16b620e98dda904845c9502a9b
shaqayq <shaqayq.darwazi@gmail.com>
2023-02-05 12:45:38 +0430

    create jobs.create page

---
+++ resources/views/jobs/create.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.app')
+@section('content')
+ <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0 text-dark">ایجاد وظیفه جدید</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-left">
+              <li class="breadcrumb-item"><a href="#">وظیفه ها</a></li>
+              <li class="breadcrumb-item active">داشبورد ورژن 2</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+    <section class="content">
+      <div class="container-fluid">
+ <div class="card card-success card-outline">
+	
+	<!-- /.card-header -->
+	<!-- form start -->
+	<form method="post" action="{{route('job.store')}}" enctype="multipart/form-data">
+		  @csrf
+		<div class="card-body">
+
+			<div class="row">
+				<div class="col-lg-5">
+			<div class="form-group">
+				<label for="exampleInputEmail1">نام وظیفه</label>
+				<input type="text" name="pr_name" class="form-control"  placeholder="نام وظیفه را وارد کنید!">
+				<b style="color: red">{{$errors->first('pr_name')}}</b>
+			</div>
+		</div>
+		<div class="col-lg-5">
+			<div class="form-group">
+				<label for="exampleInputPassword1">نام وظیفه به انگلیسی</label>
+				<input type="text"  name="en_name" class="form-control" placeholder="وظیفه را به انگلیسی وارد کنید!">
+				<b style="color: red">{{$errors->first('en_name')}}</b>
+
+			</div>
+		</div>
+		</div>
+		<div class="form-group">
+			<button type="submit" class="btn btn-success">تایید</button>
+		</div>
+	</form>
+	</div>
+	</form>
+	   </div><!-- /.container-fluid -->
+    </section>
+@endsection
