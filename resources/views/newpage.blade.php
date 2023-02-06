4ad17eb4846aefbe6e9941ea62c7309faab4fccc
shaqayq <shaqayq.darwazi@gmail.com>
2023-02-06 18:59:07 +0430

    create newPage view for Dashbord

---
+++ resources/views/newpage.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+@section('content')
+  <!-- Small boxes (Stat box) -->
+        <div class="row">
+           
+        <h1>New Page</h1>
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-warning">
+              <div class="inner">
+                <h3>44</h3>
+
+                <p>کاربران ثبت شده</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-person-add"></i>
+              </div>
+              <a href="#" class="small-box-footer">اطلاعات بیشتر <i class="fa fa-arrow-circle-left"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-danger">
+              <div class="inner">
+                <h3>65</h3>
+
+                <p>بازدید جدید</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-pie-graph"></i>
+              </div>
+              <a href="#" class="small-box-footer">اطلاعات بیشتر <i class="fa fa-arrow-circle-left"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+        </div>
+        <!-- /.row -->
+
+
+@endsection
