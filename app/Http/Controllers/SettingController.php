3bab9556fc6eec0a297bad342f0c3b89d1fa2b54
shaqayq <shaqayq.darwazi@gmail.com>
2023-02-06 18:55:48 +0430

    create seeting controller

---
+++ app/Http/Controllers/SettingController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+class SettingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+     public function __construct()
+    {
+        $this->middleware('auth');
+    }
+ 
+    public function index()
+    {
+         return view('setting.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request,[
+        "email"=>"required|email",
+        "password"=>"required|min:6|confirmed",
+        "password_confirmation"=>"required|min:6",
+       ]);
+
+       $user=User::find($id);
+       $user->email=$request->email;
+       $user->password=bcrypt($request->password);
+       $user->save();
+        Session()->flash('msg','ویرایش پروفایل موفقانه انجام شد.');
+       return redirect()->route('home.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
