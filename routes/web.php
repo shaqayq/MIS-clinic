532d020e2ae042d09418afd273aff131853f174f
shaqayq <shaqayq.darwazi@gmail.com>
2023-02-06 18:56:14 +0430

    add setting route

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,7 @@ Route::resource('expend','ExpendController');
 Route::resource('salary','SalaryController');
 Route::get('detail/{id}','SalaryController@show_detail');
 Route::resource('service_report','ServiceReportController');
+Route::resource('setting','SettingController');
 
 Route::get('tasks', function(){
 	return App\Task::where('completed', 1)->pluck('body');
