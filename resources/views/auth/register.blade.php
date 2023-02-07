cbc1cc941fa04031b7396ba1d6c634455610cb07
shaqayq <shaqayq.darwazi@gmail.com>
2023-02-07 20:29:43 +0430

    add registration form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title> صفحه ورود</title>
+  <title> Register Page</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -26,11 +26,81 @@
 <body class="hold-transition login-page" style="background-image: url({{asset('style/img/bg.jpg')}});">
 <div class="login-box">
   <div class="login-logo">
-    <a href="#"><b>ورود به سیستم</b></a>
+    <a href="#"><b>ثبت در سیستم</b></a>
   </div>
   <!-- /.login-logo -->
-  <div class="card">
-    
+  <div class="card" style="width: 419px">
+    <div class="card-body login-card-body">
+      <p class="login-box-msg">فرم زیر را تکمیل کنید و راجستر بزنید</p>
+
+      <form method="POST" action="{{ route('register') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">نام</label>
+
+                            <div class="col-md-8">
+                                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
+
+                                @if ($errors->has('name'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">ایمیل آدرس</label>
+
+                            <div class="col-md-8">
+                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
+
+                                @if ($errors->has('email'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">رمز عبور</label>
+
+                            <div class="col-md-8">
+                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">تایید رمز عبور</label>
+
+                            <div class="col-md-8">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-5" >
+                                <button type="submit" class="btn btn-success" style="width: 166px;">
+                                    ثبت
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+
+      
+      <p class="mb-1">
+        
+      </p>
+      
+    </div>
     <!-- /.login-card-body -->
   </div>
 </div>
